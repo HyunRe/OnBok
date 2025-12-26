59f6d03b0efc7631b890fd63e4f8919f2da0d951
HyunRe <93036571+HyunRe@users.noreply.github.com>
2025-12-27 05:42:16 +0900

    Delete demo directory

--- demo/src/main/java/com/lion/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lion.demo.repository;
-
-import com.lion.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
