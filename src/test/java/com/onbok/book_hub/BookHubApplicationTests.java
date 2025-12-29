3193359b7d1e8eb8f1459a2a90587e17954cd674
HyunRe <whwjdqls6868@naver.com>
2025-12-30 03:14:59 +0900

    init OnBok project

---
+++ src/test/java/com/onbok/book_hub/BookHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onbok.book_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
